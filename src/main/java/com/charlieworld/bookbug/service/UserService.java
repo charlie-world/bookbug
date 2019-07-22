3ed72565f2f2db980ba8af48c5c63bceb84f1c12
charlie-world <chlee7746@gmail.com>
2019-07-22 11:27:59 +0900

    Change token repo to redis

--- src/main/java/com/charlieworld/bookbug/service/UserService.java
+++ src/main/java/com/charlieworld/bookbug/service/UserService.java
@@ -2,7 +2,6 @@ package com.charlieworld.bookbug.service;
 
 import com.charlieworld.bookbug.dto.Token;
 import com.charlieworld.bookbug.entity.User;
-import com.charlieworld.bookbug.entity.UserToken;
 import com.charlieworld.bookbug.exception.CustomException;
 import com.charlieworld.bookbug.repository.UserRepository;
 import com.charlieworld.bookbug.repository.UserTokenRepository;
@@ -37,14 +36,10 @@ public class UserService {
                 .encryptedPassword(encryptedPassword)
                 .encryptKey(aes256CryptoService.getKey())
                 .build();
-        user = userRepository.save(user);
-        String token = TokenGenerator.generateToken();
-        UserToken userToken = UserToken.builder().token(token).userId(user.getUserId()).build();
-        userTokenRepository.save(userToken);
+        userRepository.save(user);
     }
 
     public Token login(String id, String password) throws CustomException {
-        Token result = null;
         Optional<User> user = userRepository.findByRealId(id);
         if (!user.isPresent()) {
             throw new CustomException(HttpStatus.NOT_FOUND, "회원정보가 없습니다. 회원가입 해주시기 바랍니다.");
@@ -54,27 +49,17 @@ public class UserService {
         if (!aes256CryptoService.encryptB64(password).equals(user.get().getEncryptedPassword())) {
             throw new CustomException(HttpStatus.UNAUTHORIZED, "비밀번호가 틀렸습니다.");
         }
-
-        Optional<UserToken> userToken = userTokenRepository.findByUserId(user.get().getUserId());
-
-        if (!userToken.isPresent()) {
-            String token = TokenGenerator.generateToken();
-            UserToken newUserToken = UserToken.builder().token(token).userId(user.get().getUserId()).build();
-            userTokenRepository.save(newUserToken);
-            result = Token.builder().token(token).build();
-        } else {
-            result = Token.builder().token(userToken.get().getToken()).build();
-        }
-
-        return result;
+        String token = TokenGenerator.generateToken();
+        userTokenRepository.save(user.get().getUserId(), token);
+        return Token.builder().token(token).build();
     }
 
     public Long authenticate(String token) throws CustomException {
-        Optional<UserToken> userTokenOpt = userTokenRepository.findByToken(token);
-        if (!userTokenOpt.isPresent()) {
+        Optional<Long> userIdOpt = userTokenRepository.get(token);
+        if (!userIdOpt.isPresent()) {
             throw new CustomException(HttpStatus.UNAUTHORIZED, "사용자 인증에 실패 하였습니다.");
         } else {
-            return userTokenOpt.get().getUserId();
+            return userIdOpt.get();
         }
     }
 }
